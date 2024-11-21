02e5ec9cd64279d8a65e4257c8ece972c9128011
Afonso Domingues <up2022207313@up.pt>
2024-11-21 23:58:29 +0000

    fixed visibility issues of non-logged user and some buttons on profile

--- resources/views/partials/user.blade.php
+++ resources/views/partials/user.blade.php
@@ -1,10 +1,12 @@
 <article class="user" data-id="{{ $user->id }}">
     <a href="{{route('profile',['user_id' => $user->id])}}">
     <section id="info">
-        <img src="https://i.pinimg.com/736x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg" alt="">
+        <img src="{{ $user->photo_url ? Storage::url($user->photo_url) : Storage::url('profile_pictures/default-profile.png') }}" alt="">
         <span id="user"><p>{{$user->username}}</p></span>
         <span id="nome"><p>{{$user->name}}</p></span>
     </section>
     </a>
+    @if (Auth::check())
     <button>Follow</button>
+    @endif
 </article>
